c43a924a2655f6d1cc34b6e5c606114a33b8108f
usermlc <75613235+usermlc@users.noreply.github.com>
2024-04-21 01:11:27 +0300

    Create Order.java

---
+++ Order.java
@@ -0,0 +1,24 @@
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String orderNumber;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "order_product",
+               joinColumns = @JoinColumn(name = "order_id"),
+               inverseJoinColumns = @JoinColumn(name = "product_id"))
+    private Set<Product> products = new HashSet<>();
+
+    // Getters and setters
+}
