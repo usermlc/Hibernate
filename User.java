25cb888993d2eb3a1e26b4bd8231b138fdad765d
usermlc <75613235+usermlc@users.noreply.github.com>
2024-04-21 01:10:49 +0300

    Create User.java

---
+++ User.java
@@ -0,0 +1,17 @@
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Order> orders = new HashSet<>();
+
+    // Getters and setters
+}
